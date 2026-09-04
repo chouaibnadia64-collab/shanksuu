ac0fcabf2f0db906c819151145bde6d0e4b76c78
chouaibnadia64-collab <chouaibnadia64@gmail.com>
2026-09-04 16:16:04 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "morvex community
+        name: "morvex community",
         ", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
         type: 4,               // Custom
